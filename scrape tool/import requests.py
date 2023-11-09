d0256df8bfad77fb0c9a2ed84349a73c9e80df69
amitbsot <69086052+amitbsot@users.noreply.github.com>
2023-11-09 23:24:19 +0200

    Update import requests.py

--- scrape tool/import requests.py
+++ scrape tool/import requests.py
@@ -8,7 +8,7 @@ def scrape_imdb(url):
     # Your scraping logic here
     # ...
 
-@app.route('/scrape', methods=['POST'])
+@app.route('/scrape', methods=['GET'])
 def scrape():
     try:
         url = request.get_json()['url']
