8fa4c477d76079231e1dea261a61833ffc4ecf17
amitbsot <69086052+amitbsot@users.noreply.github.com>
2023-11-10 00:03:17 +0200

    Update import requests.py

--- scrape tool/import requests.py
+++ scrape tool/import requests.py
@@ -1,23 +1,17 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 import requests
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-CORS(app)
-
-def scrape_imdb(url):
-    # Your scraping logic here (as previously provided)
 
 @app.route('/scrape', methods=['POST'])
 def scrape():
     try:
         url = request.get_json()['url']
-        data = scrape_imdb(url)
-        return jsonify(data)
+        response = requests.get(url)
+        response.raise_for_status()  # Raise HTTPError for bad requests
+        return jsonify(response.text)
     except Exception as e:
-        print(f"Error: {e}")
-        return jsonify({'error': 'An error occurred. Please check the URL and try again.'})
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
