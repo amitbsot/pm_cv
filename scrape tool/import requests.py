a27dd0a1b390627083d8f1e92058128cf20eda02
amitbsot <69086052+amitbsot@users.noreply.github.com>
2023-11-09 23:14:00 +0200

    Update import requests.py

--- scrape tool/import requests.py
+++ scrape tool/import requests.py
@@ -1,37 +1,21 @@
+from flask import Flask, request, jsonify
 import requests
 from bs4 import BeautifulSoup
 
+app = Flask(__name__)
+
 def scrape_imdb(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # Your scraping logic here
+    # ...
 
-    data = {}
+@app.route('/scrape', methods=['POST'])
+def scrape():
+    try:
+        url = request.get_json()['url']
+        data = scrape_imdb(url)
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
-    # These selectors might need to be updated depending on the exact structure of the IMDb page
-    selectors = {
-        "CAST": "div.credit_summary_item",
-        "CATEGORY": "div.see-more.inline.canwrap",
-        "DESCRIPTION": "div.summary_text",
-        "DIRECTOR": "div.credit_summary_item a",
-        "LANGUAGE": "div.txt-block",
-        "RATING": "div.ratingValue strong span",
-        "RELEASE_DATE": "div.txt-block",
-        "RESOLUTION": "div.txt-block",
-        "RUN_TIME": "div.txt-block time",
-        "EPISODE": "div.bp_heading",
-        "EPISODE_TITLE": "div.title_wrapper h1",
-        "SEASON": "div.bp_heading",
-        "SUBTITLE": "div.txt-block",
-        "TITLE": "div.title_wrapper h1",
-        "TYPE": "div.subtext a"
-    }
-
-    for key, selector in selectors.items():
-        element = soup.select_one(selector)
-        data[key] = element.get_text(strip=True) if element else "0"
-
-    return data
-
-url = "https://www.imdb.com/title/tt0111161/"  # replace with your IMDb link
-data = scrape_imdb(url)
-print(data)
+if __name__ == '__main__':
+    app.run(debug=True)
